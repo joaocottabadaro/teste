4f7404027a5e3e56bffbd7b2dd7e611acf00a9f8
ice <ice@lab05m35.nrc.ice.ufjf.br>
2018-08-24 20:44:53 -0300

    criando estoque

---
+++ Aulaob/src/Estoque/TesteEstoque.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estoque;
+
+/**
+ *
+ * @author ice
+ */
+public class TesteEstoque {
+    
+   
+
+    
+public static void main(String[]args)
+{
+      Estoque  produto1 = new Estoque("Impressora Jato de tinta",13,2);
+      produto1.dados();
+}
+    
+}
