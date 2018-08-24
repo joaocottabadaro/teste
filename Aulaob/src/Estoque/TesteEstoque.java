cefebae1726eeb64287ff86c71eb100a77a2a071
ice <ice@lab05m35.nrc.ice.ufjf.br>
2018-08-24 20:53:41 -0300

    criando estoque

--- Aulaob/src/Estoque/TesteEstoque.java
+++ Aulaob/src/Estoque/TesteEstoque.java
@@ -16,8 +16,18 @@ public class TesteEstoque {
     
 public static void main(String[]args)
 {
-      Estoque  produto1 = new Estoque("Impressora Jato de tinta",13,2);
+      Estoque  produto1 = new Estoque("Impressora Jato de tinta",13,6);
       produto1.dados();
+      Estoque produto2 = new Estoque("Monitor LCD 17 polegadas",11,13);
+      produto2.dados();
+      Estoque produto3 = new Estoque("Mouse óptico",6,2);
+      produto3.dados();
+      Estoque produto4 = new Estoque("TV SAMSUNG LED 60 polegadas",8,2);
+      produto1.baixarEstoque(5);
+      produto2.aumentarEstoque(7);
+      produto3.baixarEstoque(4);
+      
+      
 }
     
 }
